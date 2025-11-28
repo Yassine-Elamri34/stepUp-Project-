66e43a2399d42cf8e391c08f1935712d52b9910c
Yassine-Elamri34 <elamriyassin34@gmail.com>
2025-11-28 12:46:11 -0800

    51

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function reverseString(inputString) {
 }
 
 // --- Example Usage ---
-const original = "Hello fsgjasasdssacWorld!";
+const original = "Hello fsgjasakbhjhjsdssacWorld!";
 const reversed = reverseString(original);
 
 console.log(`Original: ${original}`);
